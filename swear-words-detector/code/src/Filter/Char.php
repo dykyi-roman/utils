3b2d841bab60eb06f9b19244025ad9482964adbf
roman.dykyi <roman.dykyi@eglobal-group.com>
2021-08-23 12:33:26 +0300

    swear words detector

--- swear-words-detector/code/src/Filter/Char.php
+++ swear-words-detector/code/src/Filter/Char.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filter;
 
 use Stringable;
+use Webmozart\Assert\Assert;
 
 final class Char implements Stringable
 {
@@ -12,6 +13,8 @@ final class Char implements Stringable
 
     public function __construct(string $value)
     {
+        Assert::maxLength($value, 1);
+
         $this->value = $value;
     }
 
